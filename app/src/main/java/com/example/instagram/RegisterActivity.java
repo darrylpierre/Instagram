dce0de93ddab99f59aca517a6bf9189cd5633888
darrylpierre <darryl9623@gmail.com>
2019-08-15 04:16:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagram/RegisterActivity.java
@@ -0,0 +1,52 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText fullname;
+    EditText email;
+    EditText password;
+    Button register;
+    TextView txt_login;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        fullname = findViewById(R.id.full_name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        txt_login = findViewById(R.id.txt_login);
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
